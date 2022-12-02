a823edd4db6fea76e055cd6804a3e4da9b1b6fea
herickdouglas <36521698+herickdouglas@users.noreply.github.com>
2022-12-02 20:00:56 -0300

    Add files via upload

---
+++ Trabalho_Tecnicas_4/src/br/edu/univas/vo/Student.java
@@ -0,0 +1,42 @@
+package br.edu.univas.vo;
+
+public class Student {
+
+	private String name;
+	private String matricula;
+	private String cpf;
+	private String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+}
